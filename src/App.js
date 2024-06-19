e47d8e8b788a211cd081d2c141991d4216ddeaef
nuwagalb <isnuwa@gmail.com>
2024-06-19 15:19:49 +0300

    set up project structure folders and files

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { Blog, Features, Footer, Header, Possibility, WhatGPT3 } from './containers';
+import { Article, Brand, CTA, Feature, NavBar } from './components';
+
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <NavBar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+};
+
+export default App;
