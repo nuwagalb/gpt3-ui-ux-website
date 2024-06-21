ac5fcc73ddd89700b8a28ecd8e8d49b6a7b922b0
nuwagalb <isnuwa@gmail.com>
2024-06-21 11:25:38 +0300

    add jsx for Blog component

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { Article } from '../../components';
 import './blog.css';
 
 const Blog = () => {
   return (
-    <div>
-      Blog
+    <div className='gpt3__blog section_padding' id='blog'>
+      <div className='gpt3__blog-heading'>
+        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container__groupA'>
+          <Article />
+        </div>
+        <div className='gpt3__blog-container__groupB'>
+        <Article />
+        <Article />
+        <Article />
+        <Article />
+        </div>
+      </div>
     </div>
   )
 }
