22bc586383efd1b5d0ac2776e9a1345e77daec0f
nuwagalb <isnuwa@gmail.com>
2024-06-21 11:49:43 +0300

    add jsx for Article component

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './article.css';
 
-const Article = () => {
+const Article = ({ imageUrl, date, text }) => {
   return (
-    <div>
-      Article
+    <div className='gpt3__blog-container__article'>
+      <div className='gpt3__blog-container__article'>
+        <img src={imageUrl} alt="blog_image" />
+      </div>
+      <div className='gpt3__blog-container__article-content'>
+        <p>{date}</p>
+        <h3>{text}</h3>
+      </div>
+      <p>Read Full Article</p>
     </div>
   )
 }
