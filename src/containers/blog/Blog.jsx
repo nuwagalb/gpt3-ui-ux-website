9769c21947e79e428b9162052ae1317bb9880463
nuwagalb <isnuwa@gmail.com>
2024-06-21 14:05:30 +0300

    update Blog component and it's css

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,15 +5,15 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div className='gpt3__blog section_padding' id='blog'>
+    <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
-        <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
+        <h1 className="gradient__text">A lot is happening, <br />We are blogging about it.</h1>
       </div>
       <div className='gpt3__blog-container'>
-        <div className='gpt3__blog-container__groupA'>
+        <div className='gpt3__blog-container_groupA'>
           <Article imageUrl={blog01} date="Jun 21, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?" />
         </div>
-        <div className='gpt3__blog-container__groupB'>
+        <div className='gpt3__blog-container_groupB'>
           <Article imageUrl={blog02} date="Jun 21, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
           <Article imageUrl={blog03} date="Jun 21, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
           <Article imageUrl={blog04} date="Jun 21, 2024" text="GPT-3 and Open  AI is the future. Let us exlore how it is?"/>
